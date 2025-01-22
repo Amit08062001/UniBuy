18006f3f2728250bd9283d5a315544f14a84278a
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2025-01-22 12:05:43 +0000

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from './Home'
 import './App.css'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as  Route, Routes } from 'react-router-dom';
 import Details from './Details'
 import Create from './Create'
 
