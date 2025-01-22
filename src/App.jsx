bc88c8ad3fe56d247ae8800f5583bfc04a4e1606
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2025-01-22 12:03:16 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from './Home'
 import './App.css'
-import {Routes,Route, HashRouter} from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Details from './Details'
 import Create from './Create'
 
@@ -10,15 +10,17 @@ function App() {
 
   return (
     <>
+   
      <div>
-    <HashRouter>
+    
      <Routes>
       <Route path='/create' element={<Create/>}></Route>
        <Route path='/' element={<Home/>}></Route>
          <Route path='/details/:id' element={<Details/>}></Route>
      </Routes>
-     </HashRouter>
+     
     </div> 
+    
     </>
   )
 }
